8bb1124b8c257269b64d32416a038b734362c431
Mihail <m0012229@gmail.com>
2020-09-19 14:32:58 +0300

    first commit

---
+++ Integer.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer15();
+        }
+        static void Integer1()
+        {
+            int L = 300;
+            int m = L / 100;
+            Console.WriteLine(m);
+        }
+        static void Integer2()
+        {
+            int M = 3000;
+            int t = M / 1000;
+            Console.WriteLine(t);
+        }
+        static void Integer3()
+        {
+            int a = 2000;
+            double k = a / 1024;
+            Console.WriteLine(k);
+        }
+        static void Integer4()
+        {
+            int a = 7;
+            int b = 2;
+            int d = a / b;
+            Console.WriteLine(d);
+        }
+        static void Integer5()
+        {
+            int a = 7;
+            int b = 2;
+            int o = a % b;
+            Console.WriteLine(o);
+        }
+        static void Integer6()
+        {
+            int a = 23;
+            int d = a / 10;
+            int e = a % 10;
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+        static void Integer7()
+        {
+            int a = 24;
+            int d = a / 10;
+            int e = a % 10;
+            int s = d + e;
+            int m = d * e;
+            Console.WriteLine(s);
+            Console.WriteLine(m);
+        }
+        static void Integer8()
+        {
+            int a = 56;
+            int d = a / 10;
+            int e = a % 10;
+            Console.Write(e);
+            Console.Write(d);
+        }
+        static void Integer9()
+        {
+            int a = 435;
+            int s = a / 100;
+            Console.WriteLine(s);
+        }
+        static void Integer10()
+        {
+            int a = 657;
+            int e = a % 100 % 10;
+            int d = a % 100 / 10;
+            Console.WriteLine(e);
+            Console.WriteLine(d);
+        }
+        static void Integer11()
+        {
+            int a = 234;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            int g = s + d + e;
+            int m = s * d * e;
+            Console.WriteLine(g);
+            Console.WriteLine(m);
+        }
+        static void Integer12()
+        {
+            int a = 456;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            Console.Write(e);
+            Console.Write(d);
+            Console.Write(s);
+        }
+        static void Integer13()
+        {
+            int a = 786;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            Console.Write(d);
+            Console.Write(e);
+            Console.Write(s);
+        }
+        static void Integer14()
+        {
+            int a = 479;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            Console.Write(e);
+            Console.Write(d);
+            Console.Write(s);
+        }
+        static void Integer15()
+        {
+            int a = 123;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            Console.Write(d);
+            Console.Write(s);
+            Console.Write(e);
+        }
+        static void Integer16()
+        {
+            int a = 264;
+            int s = a / 100;
+            int d = a % 100 / 10;
+            int e = a % 100 % 10;
+            Console.Write(s);
+            Console.Write(e);
+            Console.Write(d);
+        }
+        static void Integer17()
+        {
+            int a = 2100;
+            int d = a % 1000 / 100;
+            Console.WriteLine(d);
+        }
+        static void Integer18()
+        {
+            int a = 3590;
+            int t = a / 1000;
+            Console.WriteLine(t);
+        }
+        static void Integer19()
+        {
+            int s = 120;
+            double m = s / 60;
+            Console.WriteLine(m);
+        }
+        static void Integer20()
+        {
+
+        }
+        static void Integer21()
+        {
+
+        }
+        static void Integer22()
+        {
+
+        }
+        static void Integer23()
+        {
+
+        }
+        static void Integer24()
+        {
+            int k = 267;
+            int d = k % 7;
+            Console.WriteLine(d);
+        }
+        static void Integer25()
+        {
+            int k = 187;
+            int d = (k + 3) % 7;
+            Console.WriteLine(d);
+        }
+        static void Integer26()
+        {
+            int k = 264;
+            int d = (k + 1) % 7;
+            Console.WriteLine(d);
+        }
+        static void Integer27()
+        {
+            int k = 145;
+            int d = (k + 5) % 7;
+            Console.WriteLine(d);
+        }
+        static void Integer28()
+        {
+
+        }
+        static void Integer29()
+        {
+
+        }
+
+    }
+}
